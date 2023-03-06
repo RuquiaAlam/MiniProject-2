b972e4fcf5e885ace1acc8a3f361919b38e8f697
RuquiaAlam <114290800+RuquiaAlam@users.noreply.github.com>
2023-03-07 01:57:02 +0530

    updated 7/3/23

---
+++ app.js
@@ -0,0 +1,44 @@
+
+
+
+let selectCountry = document.querySelector('#selectCountry');
+let countryId = document.querySelector('#countryId');
+function onSelectCountry(event){
+    let index =countryId.selectedIndex;
+    let selectedCountryName =countryId[index].text;
+
+    console.log(selectedCountryName);
+    selectCountry.innerHTML = selectedCountryName;
+
+
+    
+
+}  
+
+let selectState = document.querySelector('#selectState');
+let stateId = document.querySelector('#stateId');
+function onSelectState(event){
+    let index =stateId.selectedIndex;
+    let selectedStateName =stateId[index].text;
+
+    console.log(selectedStateName);
+selectState.innerHTML = selectedStateName;
+
+
+    
+
+
+}
+let selectCity = document.querySelector('#selectCity');
+let cityId = document.querySelector('#cityId');
+function onSelectCity(event){
+    let index =cityId.selectedIndex;
+    let selectedCityName =cityId[index].text;
+
+    console.log(selectedCityName);
+    selectCity.innerHTML =  selectedCityName;
+
+
+
+
+}    
